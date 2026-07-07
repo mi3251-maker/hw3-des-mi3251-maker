643a20c7c70de24affc00c45438cbc61eac9978e
mi3251 <mi3251@g.rit.edu>
2026-07-07 18:59:12 -0400

    Complete IP permutation

---
+++ IP.py
@@ -0,0 +1,33 @@
+"""
+IP - Initial Permutation
+The first operation of DES encryption
+Author:
+"""
+
+INITIAL_PERMUTATION = [
+    [58, 50, 42, 34, 26, 18, 10, 2],
+    [60, 52, 44, 36, 28, 20, 12, 4],
+    [62, 54, 46, 38, 30, 22, 14, 6],
+    [64, 56, 48, 40, 32, 24, 16, 8],
+    [57, 49, 41, 33, 25, 17, 9, 1],
+    [59, 51, 43, 35, 27, 19, 11, 3],
+    [61, 53, 45, 37, 29, 21, 13, 5],
+    [63, 55, 47, 39, 31, 23, 15, 7],
+]
+
+
+def permute(bits):
+    """
+    Use the lookup table to produce permuted bits
+    pre-condition: input is a binary string of 64-bits
+    :param bits: a String of 64 bits
+    :return: permuted string of 64 bits
+    """
+    output = ""
+
+    for row in range(8):
+        for col in range(8):
+            index = INITIAL_PERMUTATION[row][col] - 1
+            output += bits[index]
+
+    return output
